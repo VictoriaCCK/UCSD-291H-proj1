1773b942e3d5d3026e200fb96d2391eaf26ad41d
xiaoemma6@163.com <victoriazz zj9766>
2020-04-05 12:47:55 -0700

    Initial Commit

---
+++ 291hw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  291hw1
+//
+//  Created by Chiu Chi Kwan on 2020/4/5.
+//  Copyright © 2020 ChiuChiuKwan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
